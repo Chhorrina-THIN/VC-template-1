f302071cf13e8368de47d2231b19335551e2690e
Chhorrina-THIN <chhorrina.thin@student.passerellesnumeriques.org>
2025-02-27 17:14:34 +0700

    Added Dashboard page

--- Router/route.php
+++ Router/route.php
@@ -4,6 +4,7 @@ require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
 require_once "Controllers/TableController.php";
+require_once "Controllers/DashboardController.php";
 
 
 $route = new Router();
@@ -12,5 +13,9 @@ $route->get("/", [WelcomeController::class, 'welcome']);
 
 // Table
 $route->get("/tables", [TableController::class, 'index']);
+
+// Dasboard
+$route->get("/dashboard", [DashboardController::class, 'index']);
+
 
 $route->route();
